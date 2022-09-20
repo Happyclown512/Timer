c29f931d923360b6875f99043e704ee55f7d7322
CS <happyclown512@gmail.com>
2022-09-20 12:16:43 -0500

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Caden on 9/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
